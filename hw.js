6ab68574d38db36c3d38cd9239280a353a7fc1ac
ivanl <162118702+vanyaqqe@users.noreply.github.com>
2024-07-29 21:13:50 +0200

    Add files via upload

---
+++ hw.js
@@ -0,0 +1,87 @@
+// Якщо змінна більше нуля - виведіть true, менше - false
+//Перевірте це на варіантах 1, 0, -3.
+// let value = -3;
+// if (value > 0){
+// console.log(true)
+// }
+// else if (value <= 0) {
+// console.log(false)
+// }
+// Якщо змінна ="test" - виведіть true,
+//Перевірте це на варіантах 'test'
+// "qwerty"
+//  true
+// let value = "test";
+// if (value === 'test') {
+// console.log(true)
+// } else if (value !== 'test') {
+// console.log(false)
+// }
+// Якщо змінна більше 10 - відніміть 5,
+//менше - додайте 5, результат виведіть в консоль
+//Перевірте це на варіантах 1, 10, 13.
+// let value = 13;
+// if (value < 10) {
+// console.log(value + 5)
+// }
+// else if (value > 10) {
+// console.log(value - 5)
+// }
+//Зробіть сервіс який отримує число від 1 до 12
+// виведіть місяць який дорівнює числу
+// const value = prompt("Введіть число від 1 до 12");
+// let month;
+// switch (value) {
+// case "1":
+// month = "СІЧЕНЬ";
+// alert (`${month}`)
+// break;
+// case "2":
+// month = "ЛЮТИЙ ";
+// alert (`${month}`)
+// break;
+// case "3":
+// month = "БЕРЕЗЕНЬ ";
+// alert (`${month}`)
+// break;
+// case "4":
+// month = "КВІТЕНЬ ";
+// alert (`${month}`)
+// break;
+// case "5":
+// month = "ТРАВЕНЬ ";
+// alert (`${month}`)
+// break;
+// case "6":
+// month = "ЧЕРВЕНЬ ";
+// alert (`${month}`)
+// break;
+// case "7":
+// month = "ЛИПЕНЬ ";
+// alert (`${month}`)
+// break;
+// case "8":
+// month = "СЕРПЕНЬ ";
+// alert (`${month}`)
+// break;
+// case "9":
+// month = "ВЕРЕСЕНЬ ";
+// alert (`${month}`)
+// break;
+// case "10":
+// month = "ЖОВТЕНЬ";
+// alert (`${month}`)
+// break;
+// case "11":
+// month = "ЛИСТОПАД ";
+// alert (`${month}`)
+// break;
+// case "12":
+// month = "ГРУДЕНЬ ";
+// alert (`${month}`)
+// break;
+// }
+//Зробіть сервіс який отримує тризначне число
+//Поверніть користувачу сумму цих чисел
+// Я не знаю як зробити
+    
